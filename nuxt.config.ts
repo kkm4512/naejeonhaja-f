4bcad45d56c67c2ba2494ee1af8048e8e6dc83be
kkm4512 <nayoun440@naver.com>
2024-12-17 22:02:07 +0900

    fix. vercel 배포 조회수 환경 설정 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { Analytics } from '@vercel/analytics/nuxt'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   css: ['~/assets/css/main.css'],
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt',Analytics ],
   devtools: { enabled: true },
   postcss: {
     plugins: {
@@ -14,5 +15,5 @@ export default defineNuxtConfig({
     public: {
       baseApi: 'https://server.naejeonhaja.com/api/v1', // API 베이스 URL 등도 여기에 정의할 수 있어
     }
-  },  
+  },
 })
