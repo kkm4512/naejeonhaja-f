f4563adcf78415124fbc19802a9c550699abe517
kkm4512 <nayoun440@naver.com>
2024-12-17 21:52:50 +0900

    fix. title 수정

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <script setup>
 useHead({
-  title: '게임 내전 플랫폼',
+  title: '내전하자',
   meta: [
     { name: 'description', content: '5:5 팀 내전을 위한 실시간 매칭 및 경기 관리 플랫폼. 팀을 만들고 전략적으로 내전을 즐기세요.' },
     { name: 'keywords', content: '5:5 내전, 게임 매칭, 팀 매칭, 실시간 경기 관리, 내전 플랫폼, 팀 내전' },
