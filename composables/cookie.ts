ac2fb1229d93e46f7c151985632c38fd50f47503
kkm4512 <nayoun440@naver.com>
2024-12-18 20:19:04 +0900

    debug. 삭제

--- composables/cookie.ts
+++ composables/cookie.ts
@@ -3,6 +3,5 @@ import { useRuntimeConfig } from '#imports';
 export const removeCookie = () => {
   const config = useRuntimeConfig();
   const domain = config.public.cookieDomain;
-  console.log("도매인 찍어보가 : " + domain);
   document.cookie = `Authorization=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; ${domain ? `domain=${domain}` : ''}`;
 };
