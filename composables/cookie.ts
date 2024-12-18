a86ddad21ea34e5be82df2d1e1b440121da41ccf
kkm4512 <nayoun440@naver.com>
2024-12-18 13:29:17 +0900

    feat. 전체적인 prd,dev 설정완료

--- composables/cookie.ts
+++ composables/cookie.ts
@@ -1,4 +1,8 @@
+import { useRuntimeConfig } from '#imports';
+
 export const removeCookie = () => {
-    document.cookie = "Authorization=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=naejeonhaja.com";
-  }
-  
+  const config = useRuntimeConfig();
+  const domain = config.public.cookieDomain;
+
+  document.cookie = `Authorization=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; ${domain ? `domain=${domain}` : ''}`;
+};
