5710120b1ec8c1169798517e9b2ade99425a69d4
kkm4512 <nayoun440@naver.com>
2024-12-18 17:24:09 +0900

    fix cookie remove

--- composables/cookie.ts
+++ composables/cookie.ts
@@ -4,5 +4,5 @@ export const removeCookie = () => {
   const config = useRuntimeConfig();
   const domain = config.public.cookieDomain;
 
-  document.cookie = `Authorization=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; ${domain ? `domain=${domain}` : ''}`;
+  document.cookie = `Authorization=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 };
